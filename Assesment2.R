6b49c092032c44d6be091fc95f024f8d0bd39073
Didar SINGH <didarbrar123@gmail.com>
2025-10-15 14:27:39 +0100

    upto step 2

--- Assesment2.R
+++ Assesment2.R
@@ -31,6 +31,56 @@ seir<-function(n=10000,ni=10,nt=100,gamma=1/3,delta=2/3,bmu=0.5,bsc=0.05){
  ##seir   
 }
 seir(n=10000,ni=10,nt=100,gamma=1/3,delta=1/5,bmu=5e-5,bsc=1e-5)$S
-  
-  
-  
+
+## step-1
+
+#-----------------------
+
+
+## n=total number of people 
+n<-1000
+##here we create a h vector of length and which contains the household ID for each person
+
+## in single line by help of rep and sample
+
+## maximum number of people in  household is five
+
+## here we set.seed to generates a same number,Every time we run the code.
+
+set.seed(9783)
+hmax<-5
+h<-rep(c(1:n),sample(1:hmax,n,replace=TRUE))[1:n]
+length(h)
+head(h,50)
+h[1000]
+
+## step-2 ___________________
+
+##-------------------
+
+get.net<-function(Beta,h,nc){
+  mylist<-vector("list",n)
+  B<-sum(Beta)/n
+  for (i in  1:n){
+    link<-c()
+    for (j in 1:n){
+      u<-runif(1,0,1)
+      p=(nc*Beta[i]*Beta[j])/((B**2)*(n-1))
+      if(h[i]!=h[j]&u<p){
+        link<-c(link,j)
+      }
+    }
+    mylist[[i]]<-link
+  }
+  return (mylist)
+}
+
+set.seed(42)
+n <- 10
+Beta <- runif(n, 0.5, 1.5)
+h <- c(1,1,2,2,3,3,4,4,5,5)
+
+contacts <- get.net(Beta, h, nc = 15)
+contacts
+
+
